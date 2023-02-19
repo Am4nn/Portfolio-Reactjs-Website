08e12ed0bbfb70aea306ef4598d7b7f9a0b326fe
Aman Arya <125aryaaman@gmail.com>
2023-02-19 14:00:12 +0530

    correction in DecoderText

--- src/components/DecoderText/DecoderText.js
+++ src/components/DecoderText/DecoderText.js
@@ -6,93 +6,95 @@ import { classes } from '../../utils/style';
 import styles from './DecoderText.module.css';
 
 const hindi = [
-  'क', 'ख', 'ग', 'घ', 'ङ',
-  'च', 'छ', 'ज', 'झ', 'ञ',
-  'ट', 'ठ', 'ड', 'ढ', 'ण',
-  'त', 'थ', 'द', 'ध', 'न',
-  'प', 'फ', 'ब', 'भ', 'म',
-  'य', 'र', 'ल', 'व',
-  'श', 'ष', 'स', 'ह', 'क्ष',
-  'त्र', 'र',
+    'क', 'ख', 'ग', 'घ', 'ङ',
+    'च', 'छ', 'ज', 'झ', 'ञ',
+    'ट', 'ठ', 'ड', 'ढ', 'ण',
+    'त', 'थ', 'द', 'ध', 'न',
+    'प', 'फ', 'ब', 'भ', 'म',
+    'य', 'र', 'ल', 'व',
+    'श', 'ष', 'स', 'ह', 'क्ष',
+    'त्र', 'र',
 ];
 
-// const glyphs = hindi.concat(chinese);
 const glyphs = hindi;
 
 const CharType = {
-  Glyph: 'glyph',
-  Value: 'value',
+    Glyph: 'glyph',
+    Value: 'value',
 };
 
 function shuffle(content, output, position) {
-  return content.map((value, index) => {
-    if (index < position) {
-      return { type: CharType.Value, value };
-    }
+    return content.map((value, index) => {
+        if (index < position) {
+            return { type: CharType.Value, value };
+        }
 
-    if (position % 1 < 0.5) {
-      const rand = Math.floor(Math.random() * glyphs.length);
-      return { type: CharType.Glyph, value: glyphs[rand] };
-    }
+        if (position % 1 < 0.5) {
+            const rand = Math.floor(Math.random() * glyphs.length);
+            return { type: CharType.Glyph, value: glyphs[rand] };
+        }
 
-    return { type: CharType.Glyph, value: output[index].value };
-  });
+        return { type: CharType.Glyph, value: output[index].value };
+    });
 }
 
+// const springConfig = { stiffness: 20, damping: 20 };
+const springConfig = { stiffness: 8, damping: 5 };
+
 const DecoderText = memo(
-  ({ text, start = true, delay: startDelay = 0, className, ...rest }) => {
-    const output = useRef([{ type: CharType.Glyph, value: '' }]);
-    const container = useRef();
-    const reduceMotion = useReducedMotion();
-    const decoderSpring = useSpring(0, { stiffness: 20, damping: 20 });
+    ({ text, start = true, delay: startDelay = 0, className, ...rest }) => {
+        const output = useRef([{ type: CharType.Glyph, value: '' }]);
+        const container = useRef();
+        const reduceMotion = useReducedMotion();
+        const decoderSpring = useSpring(0, springConfig);
 
-    useEffect(() => {
-      const containerInstance = container.current;
-      const content = text.split('');
-      let animation;
+        useEffect(() => {
+            const containerInstance = container.current;
+            const content = text.split('');
+            let animation;
 
-      const renderOutput = () => {
-        const characterMap = output.current.map(item => {
-          return `<span class="${styles[item.type]}">${item.value}</span>`;
-        });
+            const renderOutput = () => {
+                const characterMap = output.current.map(item => {
+                    return `<span class="${styles[item.type]}">${item.value}</span>`;
+                });
 
-        containerInstance.innerHTML = characterMap.join('');
-      };
+                containerInstance.innerHTML = characterMap.join('');
+            };
 
-      const unsubscribeSpring = decoderSpring.onChange(value => {
-        output.current = shuffle(content, output.current, value);
-        renderOutput();
-      });
+            const unsubscribeSpring = decoderSpring.onChange(value => {
+                output.current = shuffle(content, output.current, value);
+                renderOutput();
+            });
 
-      const startSpring = async () => {
-        await delay(startDelay);
-        decoderSpring.set(content.length);
-      };
+            const startSpring = async () => {
+                await delay(startDelay);
+                decoderSpring.set(content.length);
+            };
 
-      if (start && !animation && !reduceMotion) {
-        startSpring();
-      }
+            if (start && !animation && !reduceMotion) {
+                startSpring();
+            }
 
-      if (reduceMotion) {
-        output.current = content.map((value, index) => ({
-          type: CharType.Value,
-          value: content[index],
-        }));
-        renderOutput();
-      }
+            if (reduceMotion) {
+                output.current = content.map((value, index) => ({
+                    type: CharType.Value,
+                    value: content[index],
+                }));
+                renderOutput();
+            }
 
-      return () => {
-        unsubscribeSpring?.();
-      };
-    }, [decoderSpring, reduceMotion, start, startDelay, text]);
+            return () => {
+                unsubscribeSpring?.();
+            };
+        }, [decoderSpring, reduceMotion, start, startDelay, text]);
 
-    return (
-      <span className={classes(styles.text, className)} {...rest}>
-        <VisuallyHidden className={styles.label}>{text}</VisuallyHidden>
-        <span aria-hidden className={styles.content} ref={container} />
-      </span>
-    );
-  }
+        return (
+            <span className={classes(styles.text, className)} {...rest}>
+                <VisuallyHidden className={styles.label}>{text}</VisuallyHidden>
+                <span aria-hidden className={styles.content} ref={container} />
+            </span>
+        );
+    }
 );
 
 
