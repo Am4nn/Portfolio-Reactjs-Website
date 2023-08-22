9ee63ce3829a15060a0c67ca4e551aa85bffdbea
Aman Arya <125aryaaman@gmail.com>
2023-08-23 03:51:39 +0530

    changed the trust proxy value to suit onrender

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const express = require('express');
 const app = express();
 
 // since hosting on onrender.com, we need to trust the proxy
-app.set('trust proxy', 1);
+app.set('trust proxy', 2);
 
 // parse json request body
 app.use(express.json());
@@ -59,12 +59,14 @@ app.post("/api/contact", async (req, res) => {
     }
 });
 
-// Serve Static Assets
-// Set Static Folder
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-);
+// Serve Static Assets only if specified
+if (process.env.NO_CLIENT !== 'true') {
+    // Set Static Folder
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, async () => {
