940dd7316bdbc3708979572f87f506238674af60
Aman Arya <125aryaaman@gmail.com>
2023-09-04 06:39:34 +0530

    refactored server code

--- server.js
+++ server.js
@@ -2,6 +2,57 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
+const nodemailer = require('nodemailer');
+
+/** @type {nodemailer.Transporter} */
+let transporter = null;
+
+const connectNodeMailer = () => {
+    return new Promise((resolve, reject) => {
+        try {
+            // create reusable transporter object using the default SMTP transport
+            transporter = nodemailer.createTransport({
+                port: process.env.MAIL_PORT,
+                service: process.env.MAIL_SERVICE,
+                host: process.env.MAIL_HOST,
+                auth: {
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASSWORD,
+                },
+                secure: true,
+            });
+            resolve();
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+const mailTempltePlainText = ({ email, message }) => `${email}: ${message}`;
+const mailTemplateHTML = ({ email, message }) => `<h3><b>Email: ${email}</b></h3> <br> <div>Message: ${message}</div>`;
+const myEmail = process.env.MY_EMAIL;
+
+const sendMessageToMe = async ({ email, message }) => {
+
+    /** @type {nodemailer.Options} */
+    const mailData = {
+        from: 'Portfolio Website <portfolio-website@gmail.com>',
+        to: myEmail,
+        subject: 'Recieved a message from your portfolio website',
+        text: mailTempltePlainText({ email, message }),
+        html: mailTemplateHTML({ email, message }),
+    };
+
+    return new Promise((resolve, reject) => {
+        transporter.sendMail(mailData, function (err, info) {
+            if (err) reject(err)
+            else resolve(info);
+        });
+    })
+}
+
+// ##############################################################################
+
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const path = require('path');
@@ -9,7 +60,6 @@ const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const hpp = require('hpp');
 const rateLimit = require("express-rate-limit");
-const { connectNodeMailer, sendMessageToMe } = require('./utils/mail');
 
 const express = require('express');
 const app = express();
