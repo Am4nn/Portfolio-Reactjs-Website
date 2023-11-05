010a50ac3a9accb53ec70eddffb07b04f2532716
Aman Arya <125aryaaman@gmail.com>
2023-11-05 21:16:31 +0530

    reverted logging commit

--- api/contact.js
+++ api/contact.js
@@ -1,16 +1,4 @@
 const nodemailer = require('nodemailer');
-const { Console } = require("console");
-const fs = require("fs");
-const path = require("path");
-
-const stdoutDir = path.join(__dirname, "./server.logs");
-
-const ConsoleLogger = new Console({
-    stdout: fs.createWriteStream(stdoutDir, { flags: 'a' })
-});
-
-const dateTimeNowFormated = () => ((new Date()).toLocaleString(undefined, { timeZone: 'Asia/Kolkata' }));
-const logger = (...args) => ConsoleLogger.log(...args, dateTimeNowFormated());
 
 const mailTempltePlainText = ({ email, message }) => `${email}: ${message}`;
 const mailTemplateHTML = ({ email, message }) => `<h3><b>Email: ${email}</b></h3> <br> <div>Message: ${message}</div>`;
@@ -45,28 +33,20 @@ const sendMessageToMe = async ({ email, message }) => {
 
 // ##############################################################################
 
-/**
- * 
- * @param {Request} request 
- * @param {*} response 
- * @returns 
- */
 export default async function handler(request, response) {
-
-    logger(request.method, "/api/contact", request.url);
 
     if (request.method === "POST") {
         const { email, message } = request.body;
         try {
-            logger("Sending Mail: ", { email, message });
+            console.log("Sending Mail: ", { email, message });
             const sendStatusRes = await sendMessageToMe({ email, message });
             if (sendStatusRes.rejected.length > 0) {
-                logger("Mail Rejected: ", sendStatusRes);
+                console.error("Mail Rejected: ", sendStatusRes);
                 return response.status(500).json("Mail Rejected");
             }
             return response.status(200).json("Thank you for contacting me. I will get back to you as soon as possible!");
         } catch (error) {
-            logger("Internal Server Error: ", error);
+            console.error("Internal Server Error: ", error);
             return response.status(500).json(error || "Internal Server Error");
         }
 
